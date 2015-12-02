7ef4b4ab237ed31d908f586ab469c73b1981e02b
Sekai Kyoretsuna <sekai.kyoretsuna@gmail.com>
2015-12-02 13:20:22 -0500

    Start to add NodeIf

---
+++ Score/Front/Parse/SyntaxTree/NodeIf.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Score.Front.Parse.SyntaxTree
+{
+    internal sealed class NodeIf
+    {
+        public NodeExpr condition;
+        public List<Node> pass;
+        public List<Node> fail;
+    }
+}
