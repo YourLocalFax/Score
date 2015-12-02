18b77ba832f89bf17b8bd4cf888eb13d41eb3412
Sekai Kyoretsuna <sekai.kyoretsuna@gmail.com>
2015-12-02 13:29:46 -0500

    Small updates

--- Score/Front/Parse/SyntaxTree/NodeIf.cs
+++ Score/Front/Parse/SyntaxTree/NodeIf.cs
@@ -5,6 +5,7 @@ namespace Score.Front.Parse.SyntaxTree
     internal sealed class NodeIf
     {
         public NodeExpr condition;
+        // TODO(kai): NodeBlock or something
         public List<Node> pass;
         public List<Node> fail;
     }
