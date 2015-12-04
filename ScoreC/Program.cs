64f3ee712f88c65d398ed48ef3e6b1586e4a1b50
Sekai Kyoretsuna <sekai.kyoretsuna@gmail.com>
2015-12-04 10:27:42 -0500

    Add compiler entry point program and begin tests

---
+++ ScoreC/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Lex;
+using Log;
+using Source;
+
+namespace ScoreC
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = @"D:\projects\c#\Score\TEST_FILES\lex_test.score";
+
+            var log = new DetailLogger();
+
+            var lexer = new Lexer();
+            var tokens = lexer.GetTokens(log, fileName);
+
+            while (tokens.HasCurrent)
+            {
+                Console.WriteLine(tokens.Current.value);
+                tokens.Advance();
+            }
+
+            if (log.HasErrors)
+            {
+                Fail(log);
+                return;
+            }
+
+            Wait();
+        }
+
+        private static void Wait()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private static void Fail(DetailLogger log)
+        {
+            log.Print(Console.Out);
+            Console.WriteLine(Environment.NewLine + "Compilation failed.");
+            Console.ReadLine();
+        }
+    }
+}
