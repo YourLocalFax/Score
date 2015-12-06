87cbf7a71cad2a9e980a7732de10590e5c694e9e
Sekai Kyoretsuna <sekai.kyoretsuna@gmail.com>
2015-12-06 12:15:02 -0500

    Update type system

---
+++ Ty/TyBuiltin.cs
@@ -0,0 +1,88 @@
+﻿using System.Diagnostics;
+
+namespace Ty
+{
+    public abstract class BuiltinTyRef : TyRef
+    {
+        public static BuiltinTyRef GetForPrimitive(string name)
+        {
+            switch (name)
+            {
+                case "bool": return TyBool.BoolTy;
+                case "i8":   return TyInt.Int8Ty;
+                case "u8":   return TyUint.Uint8Ty;
+                case "i16":  return TyInt.Int16Ty;
+                case "u16":  return TyUint.Uint16Ty;
+                case "i32":  return TyInt.Int32Ty;
+                case "u32":  return TyUint.Uint32Ty;
+                case "i64":  return TyInt.Int64Ty;
+                case "u64":  return TyUint.Uint64Ty;
+                case "f16":  return TyFloat.Float16Ty;
+                case "f32":  return TyFloat.Float32Ty;
+                case "f64":  return TyFloat.Float64Ty;
+                default:
+                    Debug.Assert(false, "This should never happen. If it does, you missed a case for primitive types.");
+                    return null;
+            }
+        }
+    }
+
+    public sealed class TyVoid : BuiltinTyRef
+    {
+        public static readonly TyVoid VoidTy = new TyVoid();
+
+        private TyVoid() { }
+
+        public override string ToString() => "()";
+    }
+
+    public sealed class TyBool : BuiltinTyRef
+    {
+        public static readonly TyBool BoolTy = new TyBool();
+
+        private TyBool() { }
+
+        public override string ToString() => "bool";
+    }
+
+    public class TyInt : BuiltinTyRef
+    {
+        public static readonly TyInt Int8Ty = new TyInt(8);
+        public static readonly TyInt Int16Ty = new TyInt(16);
+        public static readonly TyInt Int32Ty = new TyInt(32);
+        public static readonly TyInt Int64Ty = new TyInt(64);
+
+        private TyInt(uint bitWidth) { BitWidth = bitWidth; }
+
+        public uint BitWidth { get; private set; }
+
+        public override string ToString() => "i" + BitWidth;
+    }
+
+    public class TyUint : BuiltinTyRef
+    {
+        public static readonly TyUint Uint8Ty = new TyUint(8);
+        public static readonly TyUint Uint16Ty = new TyUint(16);
+        public static readonly TyUint Uint32Ty = new TyUint(32);
+        public static readonly TyUint Uint64Ty = new TyUint(64);
+
+        private TyUint(uint bitWidth) { BitWidth = bitWidth; }
+
+        public uint BitWidth { get; private set; }
+
+        public override string ToString() => "i" + BitWidth;
+    }
+
+    public sealed class TyFloat : BuiltinTyRef
+    {
+        public static readonly TyFloat Float16Ty = new TyFloat(16);
+        public static readonly TyFloat Float32Ty = new TyFloat(32);
+        public static readonly TyFloat Float64Ty = new TyFloat(64);
+
+        private TyFloat(uint bitWidth) { BitWidth = bitWidth; }
+
+        public uint BitWidth { get; private set; }
+
+        public override string ToString() => "f" + BitWidth;
+    }
+}
---
+++ Ty/TyRef.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ty
+{
+    public abstract class TyRef
+    {
+        public bool IsVoid => (this as BuiltinTyRef) is TyVoid;
+    }
+
+    public sealed class InferTyRef : TyRef
+    {
+        public static readonly InferTyRef InferTy = new InferTyRef();
+
+        private InferTyRef() { }
+    }
+
+    public sealed class FnTyRef : TyRef
+    {
+        public readonly List<TyRef> parameters;
+        public readonly TyRef returnParameter;
+
+        public FnTyRef(List<TyRef> parameters, TyRef returnParameter)
+        {
+            this.parameters = parameters;
+            this.returnParameter = returnParameter;
+        }
+    }
+
+    public sealed class PointerTyRef : TyRef
+    {
+        public readonly TyRef ty;
+        public readonly bool isMut;
+
+        public PointerTyRef(TyRef ty, bool isMut)
+        {
+            this.ty = ty;
+            this.isMut = isMut;
+        }
+
+        public override string ToString() =>
+            string.Format("^{0}{1}", isMut ? "mut " : "", ty.ToString());
+    }
+
+    // TODO(kai): this is a temporary path type, so shh
+    public sealed class PathTyRef : TyRef
+    {
+        public readonly string name;
+
+        public TyRef Ty { get; private set; }
+        public bool Resolved => Ty != null;
+
+        public PathTyRef(string name)
+        {
+            this.name = name;
+        }
+
+        public void Resolve(TyRef ty)
+        {
+            if (ty is PathTyRef)
+            {
+                var path = ty as PathTyRef;
+                if (!path.Resolved)
+                    throw new ArgumentException("Cannot resolve this type with an unresolved path type.");
+                Resolve(path.Ty);
+            }
+            else Ty = ty;
+        }
+
+        public override string ToString() => name;
+    }
+}
