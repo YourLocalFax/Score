ec1e56fbbdae0a201e82b4f31ea60f73e0670f1f
Sekai Kyoretsuna <sekai.kyoretsuna@gmail.com>
2015-12-06 12:37:27 -0500

    Update modifiers

--- Ast/Data/Modifiers.cs
+++ Ast/Data/Modifiers.cs
@@ -7,53 +7,76 @@ using Source;
 
 namespace Ast.Data
 {
-    // TODO(kai): possibly make special cases or something for extern/export.
-    // TODO(kai): Maybe even allow string parameters to all modifiers?
-    // That'd simplify the problem a lot.
-
-    public sealed class Modifiers : IEnumerable<Token>
+    public enum ModifierType
     {
-        public readonly List<Token> modifiers = new List<Token>();
+        //PUB,
+        //PRIV,
+        //INTERN,
+        EXTERN,
+        //EXPORT
+    }
+
+    public sealed class Modifier
+    {
+        public readonly Token token;
+
+        public readonly ModifierType type;
+        public readonly Spanned<string> optArg;
+
+        public Span Span =>
+            optArg != null ? new Span(token.span.fileName, token.span.start, optArg.span.end) : token.span;
+
+        public Modifier(Token token, Spanned<string> optArg)
+        {
+            this.token = token;
+            type = (ModifierType)Enum.Parse(typeof(ModifierType), token.Image.ToUpper());
+            this.optArg = optArg;
+        }
+    }
+
+    public sealed class Modifiers : IEnumerable<Modifier>
+    {
+        public readonly List<Modifier> modifiers = new List<Modifier>();
 
         public Modifiers()
         {
         }
 
-        public void Add(Token modifier) => modifiers.Add(modifier);
-        public void ForEach(Action<Token> action) => modifiers.ForEach(action);
+        public void Add(Token token, Spanned<string> optArg) => modifiers.Add(new Modifier(token, optArg));
+        public void ForEach(Action<Modifier> action) => modifiers.ForEach(action);
 
         // TODO(kai): implement these later
 
-        public List<Token> GetDuplicates()
+        public List<Modifier> GetDuplicates()
         {
             // This will get all modifiers that have been given more than once.
             // e.g. pub pub fn main || -> ()
-            return new List<Token>();
+            return new List<Modifier>();
         }
 
-        public List<Token> GetConflicting()
+        public List<Modifier> GetConflicting()
         {
             // This will find conflicting modifiers.
             // e.g. pub priv fn main || -> ()
-            return new List<Token>();
+            return new List<Modifier>();
         }
 
-        public Span GetSpan(TokenType modifierType)
+        public Span GetSpan(ModifierType modifierType)
         {
-            Token token = null;
+            Modifier result = null;
             foreach (var modifier in modifiers)
                 if (modifier.type == modifierType)
                 {
-                    token = modifier;
+                    result = modifier;
                     break;
                 }
-            if (token != null)
-                return token.span;
+            if (result != null)
+                return result.Span;
             // TODO(kai): maybe different exception or different method?
             throw new ArgumentException("Could not find the requested modifier.");
         }
 
-        public bool Has(TokenType modifierType)
+        public bool Has(ModifierType modifierType)
         {
             foreach (var mod in modifiers)
                 if (mod.type == modifierType)
@@ -61,7 +84,7 @@ namespace Ast.Data
             return false;
         }
 
-        public IEnumerator<Token> GetEnumerator() => modifiers.GetEnumerator();
+        public IEnumerator<Modifier> GetEnumerator() => modifiers.GetEnumerator();
         IEnumerator IEnumerable.GetEnumerator() => modifiers.GetEnumerator();
     }
 }
